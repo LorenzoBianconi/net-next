0bd323739062939711aedd6d6ec65f2e7cd02ea0
Lorenzo Bianconi <lorenzo@kernel.org>
2025-12-19 23:12:32 +0100

    net: airoha: Rename airoha_set_gdm2_loopback in airoha_enable_gdm2_loopback
    This is a preliminary patch in order to allow the user to select if the
    configured device will be used as hw lan or wan.
    Please not this patch does not introduce any logical changes, just
    cosmetic ones.
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- drivers/net/ethernet/airoha/airoha_eth.c
+++ drivers/net/ethernet/airoha/airoha_eth.c
@@ -1798,7 +1798,7 @@ static int airoha_dev_set_macaddr(struct net_device *netdev, void *p)
 	return 0;
 }
 
-static int airoha_set_gdm2_loopback(struct airoha_gdm_dev *dev)
+static int airoha_enable_gdm2_loopback(struct airoha_gdm_dev *dev)
 {
 	struct airoha_gdm_port *port = dev->port;
 	struct airoha_eth *eth = dev->eth;
@@ -1879,7 +1879,7 @@ static int airoha_dev_init(struct net_device *netdev)
 		if (!eth->ports[1]) {
 			int err;
 
-			err = airoha_set_gdm2_loopback(dev);
+			err = airoha_enable_gdm2_loopback(dev);
 			if (err)
 				return err;
 		}
