985da3a6cf5ef20f44dbb7fa72a43a793616b172
Lorenzo Bianconi <lorenzo@kernel.org>
2026-02-24 18:43:05 +0100

    net: airoha: Move airoha_qdma pointer in airoha_gdm_dev struct
    Move airoha_qdma pointer from airoha_gdm_port struct to airoha_gdm_dev
    one since the QDMA block used depends on the particular net_device
    WAN/LAN configuration and in the current codebase net_device pointer is
    associated to airoha_gdm_dev struct.
    This is a preliminary patch to support multiple net_devices connected
    to the same GDM{3,4} port via an external hw arbiter.
    
    Tested-by: Xuegang Lu <xuegang.lu@airoha.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- drivers/net/ethernet/airoha/airoha_ppe.c
+++ drivers/net/ethernet/airoha/airoha_ppe.c
@@ -84,9 +84,9 @@ static u32 airoha_ppe_get_timestamp(struct airoha_ppe *ppe)
 			     AIROHA_FOE_IB1_BIND_TIMESTAMP);
 }
 
-void airoha_ppe_set_cpu_port(struct airoha_gdm_port *port, u8 ppe_id, u8 fport)
+void airoha_ppe_set_cpu_port(struct airoha_gdm_dev *dev, u8 ppe_id, u8 fport)
 {
-	struct airoha_qdma *qdma = port->qdma;
+	struct airoha_qdma *qdma = dev->qdma;
 	struct airoha_eth *eth = qdma->eth;
 	u8 qdma_id = qdma - &eth->qdma[0];
 	u32 fe_cpu_port;
@@ -180,8 +180,8 @@ static void airoha_ppe_hw_init(struct airoha_ppe *ppe)
 			if (!port)
 				continue;
 
-			airoha_ppe_set_cpu_port(port, i,
-						airoha_get_fe_port(port));
+			airoha_ppe_set_cpu_port(port->dev, i,
+						airoha_get_fe_port(port->dev));
 		}
 	}
 }
@@ -1473,11 +1473,12 @@ void airoha_ppe_check_skb(struct airoha_ppe_dev *dev, struct sk_buff *skb,
 	airoha_ppe_foe_insert_entry(ppe, skb, hash, rx_wlan);
 }
 
-void airoha_ppe_init_upd_mem(struct airoha_gdm_port *port)
+void airoha_ppe_init_upd_mem(struct airoha_gdm_dev *dev)
 {
-	struct net_device *dev = netdev_from_priv(port->dev);
-	struct airoha_eth *eth = port->qdma->eth;
-	const u8 *addr = dev->dev_addr;
+	struct net_device *netdev = netdev_from_priv(dev);
+	struct airoha_gdm_port *port = dev->port;
+	struct airoha_eth *eth = dev->eth;
+	const u8 *addr = netdev->dev_addr;
 	u32 val;
 
 	val = (addr[2] << 24) | (addr[3] << 16) | (addr[4] << 8) | addr[5];
