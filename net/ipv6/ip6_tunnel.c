f1d6aba5940791baf0f9026864dfd57382dfaf82
Lorenzo Bianconi <lorenzo@kernel.org>
2026-05-03 13:36:57 +0200

    net: netfilter: add encap_proto to flow_offload_tunnel
    Add encap_proto (AF_INET or AF_INET6) to struct flow_offload_tunnel
    to allow its use as part of the hash table key during flowtable entry
    lookup.
    This is a preliminary change to support IPv4 over IPv6 tunneling via
    the flowtable infrastructure for software acceleration.
    
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@oss.qualcomm.com>

--- net/ipv6/ip6_tunnel.c
+++ net/ipv6/ip6_tunnel.c
@@ -1868,6 +1868,7 @@ static int ip6_tnl_fill_forward_path(struct net_device_path_ctx *ctx,
 		path->tun.src_v6 = fl6.saddr;
 		path->tun.dst_v6 = fl6.daddr;
 		path->tun.inner_proto = IPPROTO_IPV6;
+		path->tun.encap_proto = AF_INET6;
 		path->tun.dst = dst;
 		path->dev = ctx->dev;
 		ctx->dev = dst->dev;
